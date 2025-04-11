0226d0b98efe9b0cfaeaa4e93a360b3a2f032a7c
arnbri <brighton.a@northeastern.edu>
2025-04-11 11:42:02 +0100

    added failing test

--- test_unittest.py
+++ test_unittest.py
@@ -4,6 +4,9 @@ class TestUnitTest(unittest.TestCase):
     def test_tests_work(self):
         self.assertEqual(1+1, 2)
 
+    def test_cm_converter_happy(self):
+        self.assertEqual(convert_cm_to_feet(), 1)
+
 
 if __name__ == "__main__":
     unittest.main()
